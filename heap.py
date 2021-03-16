60437602f7e8ed85a111a1c2d190603f4bb774a4
Jeng-Yeu Shih <shihjengyeu@gmail.com>
2021-03-15 23:12:02 -0700

    Rename variables.

--- heap.py
+++ heap.py
@@ -1,12 +1,12 @@
 # Implementation of min heap.
 class Heap:
     def __init__(self, iterable):
-        self.heap = [item for item in iterable]
+        self.hp = [item for item in iterable]
         self._heapify()
 
     # Get size of the heap.
     def size(self):
-        return len(self.heap)
+        return len(self.hp)
 
     # Check is the node of idx is a root or not.
     def _is_root(self, idx):
@@ -35,36 +35,35 @@ class Heap:
             for i in range(self.size() // 2 - 1, -1, -1):
                 self._heapify(i)
         else:
-            if not self._is_leaf(idx):
-                if self.heap[idx] > self.heap[self._left_child(idx)] or self._right_chid(idx) < self.size() and self.heap[idx] > self.heap[self._right_chid(idx)]:
-                    if self._right_chid(idx) < self.size() and self.heap[self._right_chid(idx)] < self.heap[self._left_child(idx)]:
-                        self._swap(idx, self._right_chid(idx))
-                        self._heapify(self._right_chid(idx))
-                    else:
-                        self._swap(idx, self._left_child(idx))
-                        self._heapify(self._left_child(idx))
+            if not self._is_leaf(idx) and self.hp[idx] > self.hp[self._left_child(idx)] or self._right_chid(idx) < self.size() and self.hp[idx] > self.hp[self._right_chid(idx)]:
+                if self._right_chid(idx) < self.size() and self.hp[self._right_chid(idx)] < self.hp[self._left_child(idx)]:
+                    self._swap(idx, self._right_chid(idx))
+                    self._heapify(self._right_chid(idx))
+                else:
+                    self._swap(idx, self._left_child(idx))
+                    self._heapify(self._left_child(idx))
 
     # Insert an element into the heap.
     def push(self, element):
-        self.heap.append(element)
+        self.hp.append(element)
         cur = self.size() - 1
-        while not self._is_root(cur) and self.heap[cur] < self.heap[self._parent(cur)]:
+        while not self._is_root(cur) and self.hp[cur] < self.hp[self._parent(cur)]:
             self._swap(cur, self._parent(cur))
             cur = self._parent(cur)
 
     # Remove the element from the heap, and return it.
     def pop(self):
-        if self.heap:
+        if self.hp:
             self._swap(0, self.size() - 1)
-            element = self.heap.pop()
+            element = self.hp.pop()
             self._heapify()
             return element
 
     # Retrieve, but not remove, the root of the heap.
     def peek(self):
-        if self.heap:
-            return self.heap[0]
+        if self.hp:
+            return self.hp[0]
 
     # Swap the values of two nodes.
     def _swap(self, p, q):
-        self.heap[p], self.heap[q] = self.heap[q], self.heap[p]
+        self.hp[p], self.hp[q] = self.hp[q], self.hp[p]
